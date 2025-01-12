633a81ee598cfeda004b9a866c1800aea5518278
Vivek <ojhavivek24@gmail.com>
2025-01-12 18:05:19 +0530

    added shape shifting effect

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+const gridContainerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const gridSquareVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 const App = () => {
   return (
-    <div>
-      <section></section>
-    </div>
-  )
-}
+    <div className="flex flex-col gap-10 overflow-x-hidden">
+      <motion.section
+        variants={gridContainerVariants}
+        initial="hidden"
+        animate="show"
+        className="grid grid-cols-3 p-10 gap-10"
+      >
+        
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
+        >
+          <motion.div 
+          className="w-20 h-20 bg-stone-100 rounded-lg"
+          initial={{opacity:0, y:100}}
+          animate={{opacity:1, y:0}}
+          transition={{duration:1,ease:"easeOut", delay: 0.2}}
+          >
+          </motion.div>
+          <motion.div 
+          className="w-20 h-20 bg-stone-100 rounded-full"
+          initial={{opacity:0, y:-100}}
+          animate={{opacity:1, y:0}}
+          transition={{duration:1,ease:"easeOut", delay: 0.4}}
+          ></motion.div>
+        </motion.div>
 
-export default App
+        {/* Shape Shifting */}
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
+        >
+          <motion.div 
+          className="w-1/3 h-1/3 shadow-md bg-rose-400"
+          animate={{
+            scale:[1,2,2,1],
+            rotate:[0, 90, 90, 0],
+            borderRadius:["10%","10%","50%","10%"]
+          }}
+          transition={{duration:5,ease:"easeInOut", repeat:2, delay:1}}
+          ></motion.div>
+        </motion.div>
+
+
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
+        >
+          <motion.div className="bg-emerald-600 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
+          
+          ></motion.div>
+        </motion.div>
+
+
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
+        ></motion.div>
+      </motion.section>
+    </div>
+  );
+};
+
+export default App;
