44c8f433554e920deda0f1576a1fb868c05b5875
Vivek <ojhavivek24@gmail.com>
2025-01-12 18:32:04 +0530

    scroll progress

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 
 const gridContainerVariants = {
   hidden: { opacity: 0 },
@@ -16,6 +16,7 @@ const gridSquareVariants = {
 };
 
 const App = () => {
+  const {scrollYProgress : completionProgress} = useScroll()
   return (
     <div className="flex flex-col gap-10 overflow-x-hidden">
       <motion.section
@@ -97,7 +98,13 @@ const App = () => {
         <motion.div
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
-        ></motion.div>
+        >
+
+          <motion.div className="w-40 aspect-square bg-gray-50/20 rounded-xl">
+            <motion.div className="w-full bg-gray-400 rounded-xl h-full origin-bottom" style={{scaleY:completionProgress}}></motion.div>
+          </motion.div>
+
+        </motion.div>
 
         <motion.div
           variants={gridSquareVariants}
