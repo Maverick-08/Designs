98812e87bf0005fad27a7d31c3ba02aeb5fa29c8
Vivek <ojhavivek24@gmail.com>
2025-01-12 19:00:58 +0530

    added svg animation

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,4 +1,4 @@
-import { motion, useScroll } from "framer-motion";
+import { delay, motion, useScroll } from "framer-motion";
 
 const gridContainerVariants = {
   hidden: { opacity: 0 },
@@ -15,8 +15,21 @@ const gridSquareVariants = {
   show: { opacity: 1 },
 };
 
+const svgIconVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(252, 211, 77, 0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(252, 211, 77, 1)",
+  },
+};
+
 const App = () => {
-  const {scrollYProgress : completionProgress} = useScroll()
+  const { scrollYProgress: completionProgress } = useScroll();
   return (
     <div className="flex flex-col gap-10 overflow-x-hidden">
       <motion.section
@@ -99,17 +112,49 @@ const App = () => {
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
         >
-
           <motion.div className="w-40 aspect-square bg-gray-50/20 rounded-xl">
-            <motion.div className="w-full bg-gray-400 rounded-xl h-full origin-bottom" style={{scaleY:completionProgress}}></motion.div>
+            <motion.div
+              className="w-full bg-gray-400 rounded-xl h-full origin-bottom"
+              style={{ scaleY: completionProgress }}
+            ></motion.div>
           </motion.div>
-
         </motion.div>
 
         <motion.div
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"
-        ></motion.div>
+        >
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-1/2 stroke-amber-500 stroke-[0.5]"
+          >
+            <motion.path
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              variants={svgIconVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{
+                default: {
+                  duration: 2,
+                  ease: "easeInOut",
+                  delay: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+                fill: {
+                  duration: 2,
+                  ease: "easeIn",
+                  delay: 2,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+              }}
+            ></motion.path>
+          </motion.svg>
+        </motion.div>
       </motion.section>
     </div>
   );
