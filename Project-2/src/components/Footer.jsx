50254a46a549f166736741ca6296e87acf49c798
Vivek <ojhavivek24@gmail.com>
2025-02-03 16:06:44 +0530

    added footer section

---
+++ Project-2/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-16 border-t border-slate-600 flex flex-wrap'>
+      <div>Resources</div>
+      <div>Platform</div>
+      <div>Community</div>
+    </div>
+  )
+}
+
+export default Footer
